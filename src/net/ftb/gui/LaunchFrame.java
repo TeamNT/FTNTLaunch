fef4aef54d16bc729bc493f4d59afa584165c629
Fares76 <faresticha@gmail.com>
2014-01-07 20:52:53 +0100

    Release New Version

--- src/net/ftb/gui/LaunchFrame.java
+++ src/net/ftb/gui/LaunchFrame.java
@@ -138,7 +138,7 @@ public class LaunchFrame extends JFrame {
 	private static String[] dropdown_ = {"Select Profile", "Create Profile"};
 	private static JComboBox users, tpInstallLocation, mapInstallLocation;
 	private static LaunchFrame instance = null;
-	private static String version = "1.1.2";
+	private static String version = "1.1.3";
 
 	public final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);	
 
@@ -149,7 +149,7 @@ public class LaunchFrame extends JFrame {
 	public TexturepackPane tpPane;
 	public OptionsPane optionsPane;
 
-	public static int buildNumber = 112;
+	public static int buildNumber = 113;
 	public static boolean noConfig = false;
 	public static LauncherConsole con;
 	public static String tempPass = "";
