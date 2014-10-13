de6216f117708c651b7f934830cc96943e2dcb5f
Iclario <faresticha@gmail.com>
2014-10-13 02:12:39 +0200

    A small error

--- src/main/java/net/ftb/gui/dialogs/LauncherUpdateDialog.java
+++ src/main/java/net/ftb/gui/dialogs/LauncherUpdateDialog.java
@@ -55,7 +55,7 @@ public class LauncherUpdateDialog extends JDialog {
             public void actionPerformed (ActionEvent arg0) {
                 // TODO: Call new frame containing html page?
                 // TODO: beta changelogs???
-                OSUtils.browse("http://ftnt.redheberg.com/change-logs.html");//TODO update this!!!
+                OSUtils.browse("http://ftnt.redheberg.com/change-log.html");//TODO update this!!!
             }
         });
 
