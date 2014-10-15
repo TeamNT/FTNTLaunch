126986aca6d36cc468a9e66649b10672669f76e9
Julien <juju790@users.noreply.github.com>
2014-10-15 20:01:58 +0200

    Spacing buttons for the FTB pack menu

--- src/main/java/net/ftb/gui/panes/FTBPacksPane.java
+++ src/main/java/net/ftb/gui/panes/FTBPacksPane.java
@@ -162,7 +162,7 @@ public class FTBPacksPane extends AbstractModPackPane implements ILauncherPane {
         add(infoScroll);
 
         server = new JButton(I18N.getLocaleString("DOWNLOAD_SERVER"));
-        server.setBounds(420, 5, 130, 25);
+        server.setBounds(420 + 75, 5, 130, 25);
 
         //TODO: check
         server.addActionListener(new ActionListener() {
@@ -194,7 +194,7 @@ public class FTBPacksPane extends AbstractModPackPane implements ILauncherPane {
         add(server);
 
         version = new JComboBox(new String[]{});
-        version.setBounds(560, 5, 130, 25);
+        version.setBounds(560 + 140, 5, 130, 25);
         version.addActionListener(al);
         version.setToolTipText(I18N.getLocaleString("MODPACK_VERSIONS"));
         add(version);
