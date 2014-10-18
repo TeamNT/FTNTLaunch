d58c9f9baa74bed14d4ea92e641ae0eafb754dd9
Julien <juju790@users.noreply.github.com>
2014-10-18 15:15:58 +0200

    Edit NewsPane

--- src/main/java/net/ftb/gui/panes/NewsPane.java
+++ src/main/java/net/ftb/gui/panes/NewsPane.java
@@ -17,6 +17,7 @@
 package net.ftb.gui.panes;
 
 import java.awt.BorderLayout;
+import java.io.IOException;
 
 import javax.swing.JEditorPane;
 import javax.swing.JPanel;
@@ -28,62 +29,59 @@ import javax.swing.event.HyperlinkListener;
 import javax.swing.text.html.HTMLEditorKit;
 
 import net.ftb.data.Settings;
-import net.ftb.download.Locations;
 import net.ftb.gui.LaunchFrame;
-import net.ftb.util.NewsUtils;
+import net.ftb.log.Logger;
 import net.ftb.util.OSUtils;
 import net.ftb.util.OSUtils.OS;
 
 @SuppressWarnings("serial")
 public class NewsPane extends JPanel implements ILauncherPane {
-    
+	
+    private JEditorPane news;
     private JScrollPane newsPanel;
     
-    private final HTMLEditorKit news_kit = new HTMLEditorKit() {
-        {
-            this.setStyleSheet(OSUtils.makeStyleSheet("news"));
-        }
-    };
-    
-    private final JEditorPane news_pane = new JEditorPane("text/html", "") {
-        {
-            this.setEditable(false);
-            this.setEditorKit(news_kit);
-            this.addHyperlinkListener(new HyperlinkListener() {
-                @Override
-                public void hyperlinkUpdate(HyperlinkEvent e) {
-                    if (e.getEventType() == EventType.ACTIVATED) {
-                        if(e.getDescription().substring(0, 7).equals("members")) {
-                            OSUtils.browse(Locations.forum + e.getDescription());
-                        } else {
-                            OSUtils.browse(e.getDescription());
-                        }
-                    }
-                }
-            });
-        }
-    };
+    private final HTMLEditorKit news_kit = new HTMLEditorKit();
 
     public NewsPane() {
-        super(new BorderLayout());
+        super();
         
-        if(OSUtils.getCurrentOS() == OS.WINDOWS) {
-            setBorder(new EmptyBorder(-5, 0, -5, 12));
+        if (OSUtils.getCurrentOS() == OS.WINDOWS) {
+            setBorder(new EmptyBorder(-5, -25, -5, 12));
         } else {
-            setBorder(new EmptyBorder(-4, 0, -4, -2));
+            setBorder(new EmptyBorder(-4, -25, -4, -2));
         }
         
-        newsPanel = new JScrollPane(this.news_pane, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        this.add(newsPanel, BorderLayout.CENTER);
-        this.news_pane.setText(NewsUtils.getNewsHTML());
+        setLayout(new BorderLayout());
+
+        news = new JEditorPane();
+        news.setEditable(false);
+        news.setEditorKit(news_kit);
+        news.setContentType("text/html");
+        news.addHyperlinkListener(new HyperlinkListener() {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if (e.getEventType() == EventType.ACTIVATED) {
+                    OSUtils.browse(e.getURL().toString());
+                }
+            }
+        });
+        
+        newsPanel = new JScrollPane(news);
+        newsPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        newsPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        add(newsPanel, BorderLayout.CENTER);
     }
 
     @Override
     public void onVisible () {
-        Settings.getSettings().setNewsDate();
-        Settings.getSettings().save();
-        LaunchFrame.getInstance().setNewsIcon();
+        try {
+            news.setPage("http://ftnt.redheberg.com/news.html");
+            Settings.getSettings().setNewsDate();
+            Settings.getSettings().save();
+            LaunchFrame.getInstance().setNewsIcon();
+        } catch (IOException e1) {
+            Logger.logError("Erreur lors de la mise à jour des news !", e1);
+        }
     }
     
 }
