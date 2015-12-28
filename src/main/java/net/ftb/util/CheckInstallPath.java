0ba9f3cd310fc7a768617d7dced9bf9ad3f7b2ed
Farès Chati <faresticha@gmail.com>
2015-12-28 19:14:06 +0300

    Misc changes

--- src/main/java/net/ftb/util/CheckInstallPath.java
+++ src/main/java/net/ftb/util/CheckInstallPath.java
@@ -47,7 +47,7 @@ public class CheckInstallPath {
     public CheckInstallPath (String path, boolean calledFromLaunchFrame) {
         installPath = path;
         File f = new File(path);
-        String defaultLocation = "C:\\FTB";
+        String defaultLocation = "C:\\FTNT";
 
         /**
          *  Messages are shown by FirstRunDialog and by LaunchFrame
