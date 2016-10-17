d603aff753ea0bd20ef659819d52471085811b85
Farès Chati <faresticha@gmail.com>
2016-10-17 22:18:33 +0200

    Disabled RSS news

--- src/main/java/net/ftb/data/news/RSSReader.java
+++ src/main/java/net/ftb/data/news/RSSReader.java
@@ -16,76 +16,13 @@
  */
 package net.ftb.data.news;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import com.google.common.collect.Lists;
-
-import net.ftb.download.Locations;
-import net.ftb.log.Logger;
-import net.ftb.util.AppUtils;
 
 public class RSSReader
 {
 
 	public static List<NewsArticle> readRSS ()
 	{
-		try
-		{
-			List<NewsArticle> news = Lists.newArrayList();
-
-			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			URL u = new URL(Locations.feedURL);
-			HttpURLConnection conn = (HttpURLConnection)u.openConnection();
-			conn.connect();
-			if (conn.getResponseCode() != 200)
-			{
-				Logger.logWarn("News download failed");
-				AppUtils.debugConnection(conn);
-				conn.disconnect();
-				return null;
-			}
-			Document doc = builder.parse(conn.getInputStream());
-			NodeList nodes = doc.getElementsByTagName("item");
-
-			for(int i = 0; i < nodes.getLength(); i++)
-			{
-				Element element = (Element)nodes.item(i);
-				NewsArticle article = new NewsArticle();
-				article.setTitle(getTextValue(element, "title"));
-				article.setHyperlink(getTextValue(element, "link"));
-				article.setBody(getTextValue(element, "content:encoded"));
-				article.setDate(getTextValue(element, "pubDate"));
-				news.add(article);
-			}
-
-			return news;
-		}
-		catch (Exception ex)
-		{
-			Logger.logWarn("News download failed", ex);
-			return null;
-		}
+		return null;
 	}
-
-	private static String getTextValue (Element doc, String tag)
-	{
-		String value = "";
-		NodeList nl;
-		nl = doc.getElementsByTagName(tag);
-		if (nl.getLength() > 0 && nl.item(0).hasChildNodes())
-		{
-			value = nl.item(0).getFirstChild().getNodeValue();
-		}
-		return value;
-	}
-
 }
